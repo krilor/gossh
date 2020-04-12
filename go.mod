bd0cfbd98ab7598e17b8e4c929c365704eae254a
Kristoffer Lorentsen <krilor@gmail.com>
2020-04-12 10:03:26 +0200

    adds first stab at suftp package

--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/google/uuid v1.1.1 // indirect
 	github.com/lithammer/shortuuid v3.0.0+incompatible
 	github.com/pkg/errors v0.9.1
+	github.com/pkg/sftp v1.11.0
 	golang.org/x/crypto v0.0.0-20200323165209-0ec3e9974c59
 )
---
+++ rmt/suftp/suftp.go
@@ -0,0 +1,159 @@
+package suftp
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/pkg/errors"
+	"github.com/pkg/sftp"
+	"golang.org/x/crypto/ssh"
+)
+
+// Package suftp contains handy utility methods to create non-standard SFTP clients for github.com/pkg/sftp.
+//
+// The package if mostly useful for sudo-based sftp or when the remote sshd does not have an sftp subsystem configured.
+
+// NewSubsystemClient creates a new SFTP client on conn, using zero or more option
+// functions.
+//
+// Subsystem must be defined in targets sshd_config.
+func NewSubsystemClient(conn *ssh.Client, subsystem string, opts ...sftp.ClientOption) (*sftp.Client, error) {
+
+	s, err := conn.NewSession()
+	if err != nil {
+		return nil, err
+	}
+	if err := s.RequestSubsystem(subsystem); err != nil {
+		return nil, err
+	}
+	pw, err := s.StdinPipe()
+	if err != nil {
+		return nil, err
+	}
+	pr, err := s.StdoutPipe()
+	if err != nil {
+		return nil, err
+	}
+
+	return sftp.NewClientPipe(pr, pw, opts...)
+}
+
+// NewSubsystemCommandClient creates a new SFTP client on conn, using a custom subsystem command and zero or more option
+// functions.
+//
+// Command cmd can be used to specify a custom subsystem command, similar to the -s option for sftp cli.
+//
+// Specify subsystem as a path when the remote sshd does not have an sftp Subsystem configured.
+// Specify subsystem as "sudo -u [user] /path/to/sftp-server" to get SFTP with another user.
+//
+// Sudo must have NOPASSWD for the sftp-server binary.
+func NewSubsystemCommandClient(conn *ssh.Client, cmd string, opts ...sftp.ClientOption) (*sftp.Client, error) {
+
+	s, err := conn.NewSession()
+	if err != nil {
+		return nil, err
+	}
+	// TODO - use s.Start()
+	ok, err := s.SendRequest("exec", true, ssh.Marshal(struct{ Command string }{cmd}))
+	if err == nil && !ok {
+		err = fmt.Errorf("sftp: command %v failed", cmd)
+	}
+	if err != nil {
+		return nil, err
+	}
+	pw, err := s.StdinPipe()
+	if err != nil {
+		return nil, err
+	}
+	pr, err := s.StdoutPipe()
+	if err != nil {
+		return nil, err
+	}
+
+	return sftp.NewClientPipe(pr, pw, opts...)
+}
+
+// NewSudoClient creates a new SFTP client on conn, using user and sudopass for the conn user and zero or more option
+// functions.
+func NewSudoClient(conn *ssh.Client, user, sudopwd string, opts ...sftp.ClientOption) (*sftp.Client, error) {
+
+	s, err := conn.NewSession()
+	if err != nil {
+		return nil, err
+	}
+
+	promptpwd := "gimmeyourpwdnow"
+	promptsuccess := "thesudopwdwasok"
+	promtlength := 18 // prompt + newline
+
+	// serverpaths are the most likely paths to the sftp-server binary, ordered from most likely less likely
+	// paths are from https://winscp.net/eng/docs/faq_su#fn2
+	serverpaths := []string{
+		"/usr/libexec/openssh/sftp-server",
+		"/usr/lib/openssh/sftp-server",
+		"/usr/lib/sftp-server",
+		"/usr/bin/sftp-server",
+		"/bin/sftp-server",
+	}
+
+	// this command is where most of the magic happens
+	//
+	// sudo -p specifies the password prompt from sudo. The prompt is sent to stderr
+	// sudo -S dictates that password should be read from stdin
+	// sh -c 'cmd' passes cmd to sh
+	// >&2 echo "%s" echos %s to stderr ( see https://stackoverflow.com/a/23550347 )
+	// & %s is the final command to be executed, which is the path to server-path binary. It is a list of possible binary paths and it will pick the first one that exists.
+	// In total this means that we can read from stderr and write to stdin to
+	// TODO - this should probably be a separate, tested function
+	cmd := fmt.Sprintf(`sudo -p '%s\n' -S sh -c '>&2 echo "%s" & %s'`, promptpwd, promptsuccess, strings.Join(serverpaths, " 2> /dev/null || "))
+
+	stdin, err := s.StdinPipe()
+	if err != nil {
+		return nil, err
+	}
+
+	stdout, err := s.StdoutPipe()
+	if err != nil {
+		return nil, err
+	}
+
+	stderr, err := s.StderrPipe()
+	if err != nil {
+		return nil, err
+	}
+
+	err = s.Start(cmd)
+
+	if err != nil {
+		return nil, err
+	}
+
+	buf := make([]byte, promtlength)
+
+	// First read should either be success promt, password prompt or something else
+	_, err = stderr.Read(buf)
+	if err != nil {
+		return nil, errors.Wrap(err, "first stderr read failed")
+	}
+
+	switch string(buf[:promtlength-1]) { // remove newline char at end
+	case promptpwd:
+		stdin.Write([]byte(sudopwd + "\n"))
+	case promptsuccess:
+		return sftp.NewClientPipe(stdout, stdin, opts...)
+	default:
+		return nil, fmt.Errorf("unknown prompt: %s", string(buf))
+	}
+
+	// Second read cshouldan be either be success or something else. If it is not success, wrong sudo pasword is the most likely scenario.
+	_, err = stderr.Read(buf)
+	if err != nil {
+		return nil, errors.Wrap(err, "second stderr read failed")
+	}
+
+	if string(buf[:promtlength-1]) != promptsuccess {
+		return nil, errors.New("wrong sudo password")
+	}
+
+	return sftp.NewClientPipe(stdout, stdin, opts...)
+}
